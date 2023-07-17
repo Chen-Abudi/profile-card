fd7f5855caa9ff26ca83cdaa3413bd9ce90c5364
Grace Chen Abudi <cheezumcali23@gmail.com>
2023-07-17 21:27:51 +0300

    initial commit

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    // Specifies the ESLint configuration to use for Next.js
+    ignoreDuringBuilds: true,
+  },
+};
+
+module.exports = nextConfig;
